62d88d9312cd2ef7b05643bd4a1c2294c34723c6
Jay Wu <cn.fyodor@gmail.com>
2013-05-21 14:47:56 +0800

    Fix build with new C2D color converter

--- Android.mk
+++ Android.mk
@@ -6,5 +6,5 @@ endif
 
 ifneq ($(filter msm8974 msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
 include $(QCOM_MEDIA_ROOT)/mm-video/Android.mk
-include $(QCOM_MEDIA_ROOT)/libI420colorconvert/Android.mk
+include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 endif
